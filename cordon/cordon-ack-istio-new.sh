0ccb6073d47bdc1dbebabf74f22613b6f8bb785b
zhdu <zhdu@lingshi.com>
2026-06-24 16:06:11 +0800

    调试脚本

--- cordon/cordon-ack-istio-new.sh
+++ cordon/cordon-ack-istio-new.sh
@@ -4,5 +4,9 @@
 # 每10min检查内存使用率然后进行检查，封锁达到阈值的节点
 # 如果有半数以上的节点达到封锁的阈值，则触发报警
 set -euo pipefail
-KUBECTL_CMD=''
-NODE
+KUBECTL_CMD='/usr/bin/kubectl --kubeconfig=/path/to/kubeconfig'
+NODE_FilTER='' #过滤节点，分离gateway和worker节点
+CORDON_LIMIT=4
+# 日志输出在kuboard
+
+WEB_HOOK_URL=''
